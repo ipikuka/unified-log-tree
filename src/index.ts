d3f644bf2e95547d7c28fd542c960d1a8d78cf9a
talatkuyuk <talatkuyuk@gmail.com>
2026-02-22 19:35:01 +0300

    Add main code

---
+++ src/index.ts
@@ -0,0 +1,191 @@
+import type { Plugin } from "unified";
+import type { Node as UnistNode } from "unist";
+import type { Test } from "unist-util-is";
+import { is } from "unist-util-is";
+
+type Prettify<T> = { [K in keyof T]: T[K] } & {};
+
+type PartiallyRequired<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
+
+export interface Node extends UnistNode {
+  [key: string]: unknown;
+}
+
+export type UnistLogTreeOptions = {
+  depth?: number | null;
+  indentation?: number;
+  preservePositions?: boolean;
+  test?: Test;
+  preserveSubtree?: boolean;
+  label?: string;
+  enabled?: boolean;
+};
+
+const DEFAULT_SETTINGS: UnistLogTreeOptions = {
+  depth: null,
+  indentation: 2,
+  preservePositions: false,
+  test: undefined,
+  preserveSubtree: true,
+  label: undefined,
+  enabled: true,
+};
+
+type PartiallyRequiredOptions = Prettify<
+  PartiallyRequired<
+    UnistLogTreeOptions,
+    "depth" | "indentation" | "preservePositions" | "enabled"
+  >
+>;
+
+/**
+ * Debug utility plugin for unified processors.
+ *
+ * Logs the current unist tree (mdast, hast, estree, etc.)
+ * with optional filtering and formatting controls.
+ *
+ * If `test` is provided, only matching nodes and their
+ * parent chain are preserved in the output.
+ *
+ * The original tree is never mutated.
+ */
+export default function plugin(options?: UnistLogTreeOptions): Plugin<[], Node> {
+  return function attacher() {
+    const settings = Object.assign({}, DEFAULT_SETTINGS, options) as PartiallyRequiredOptions;
+
+    // early exit
+    if (!settings.enabled) {
+      return function transformer(tree: Node) {
+        return tree;
+      };
+    }
+
+    /*
+     * Type guard to check if a node is a unist Node
+     */
+    function isNode(value: unknown): value is Node {
+      return !!value && typeof value === "object" && "type" in value;
+    }
+
+    /**
+     * Builds a minimal subtree that keeps only nodes matching the test and their parent chain.
+     * It uses prune algoritm that works on children key, so with mdast and hast okey but with esast doesn't work
+     */
+    // function buildFilteredTreeEx(root: Node, test: Test): Node {
+    //   const cloned = structuredClone(root);
+
+    //   function prune(node: Node): boolean {
+    //     const matched = is(node, test);
+
+    //     if (isParent(node)) {
+    //       if (matched && settings.preserveSubtree !== false) {
+    //         return true; // don't touch subtree
+    //       }
+
+    //       node.children = node.children.filter((child) => prune(child));
+    //     }
+
+    //     if (matched) return true;
+
+    //     if (isParent(node)) {
+    //       return node.children.length > 0;
+    //     }
+
+    //     return false;
+    //   }
+
+    //   const keepRoot = prune(cloned);
+
+    //   if (!keepRoot && isParent(cloned)) {
+    //     cloned.children = [];
+    //   }
+
+    //   return cloned;
+    // }
+
+    /**
+     * Builds a minimal subtree that keeps only nodes matching the test and their parent chain.
+     * It is a universal travel algoritim works with any AST like mdast, hast, esast and others
+     */
+    function buildFilteredTree(root: Node, test: Test): Node {
+      const cloned = structuredClone(root);
+
+      function prune(node: Node): boolean {
+        const matched = is(node, test);
+
+        let hasChildMatch = false;
+
+        for (const key in node) {
+          if (key === "position") continue;
+
+          const value = node[key];
+
+          if (!value) continue;
+
+          if (Array.isArray(value)) {
+            let containsNode = false;
+
+            for (let i = value.length - 1; i >= 0; i--) {
+              const item = value[i];
+
+              /* v8 ignore next -- @preserve */
+              if (isNode(item)) {
+                containsNode = true;
+
+                const keep = prune(item);
+
+                if (!keep) {
+                  value.splice(i, 1);
+                } else {
+                  hasChildMatch = true;
+                }
+              }
+            }
+
+            if (containsNode && value.length === 0) {
+              node[key] = [];
+            }
+          } else if (isNode(value)) {
+            const keep = prune(value);
+
+            if (!keep) {
+              delete node[key];
+            } else {
+              hasChildMatch = true;
+            }
+          }
+        }
+
+        if (matched && settings.preserveSubtree !== false) {
+          return true; // don't touch subtree
+        }
+
+        if (matched) return true;
+
+        return hasChildMatch;
+      }
+
+      prune(cloned);
+      return cloned;
+    }
+
+    return function transformer(tree: Node) {
+      const targetTree = settings.test != null ? buildFilteredTree(tree, settings.test) : tree;
+
+      if (settings.label) {
+        console.log(`[unist-log-tree] ${settings.label}`);
+      }
+
+      const output = JSON.parse(
+        JSON.stringify(targetTree, (key, value) => {
+          if (!settings.preservePositions && key === "position") {
+            return undefined;
+          }
+          return value;
+        }),
+      );
+
+      console.dir(output, { depth: settings.depth });
+    };
+  };
+}
