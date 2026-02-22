02d42c5852a87498ccddf2147cda543f5b787e73
talatkuyuk <talatkuyuk@gmail.com>
2026-02-22 19:34:17 +0300

    Initial project structure

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import { defineConfig } from "eslint/config";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+import vitest from "@vitest/eslint-plugin";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default defineConfig(
+  {
+    ignores: [
+      ".DS_Store",
+      ".vscode/",
+      "archive/",
+      "coverage/",
+      "dist/",
+      "node_modules/",
+      "package-lock.json",
+    ],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    name: "javascript",
+    files: ["**/*.js"],
+    ...tseslint.configs.disableTypeChecked,
+  },
+  {
+    name: "vitest",
+    files: ["tests/**/*.spec.ts"],
+    ...vitest.configs.recommended,
+  },
+  eslintPluginPrettierRecommended,
+);
